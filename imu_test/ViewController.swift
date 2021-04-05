0177a67876e7f22094cc7c9109c8d61de59ded56
Justin Chan <chan.justin.hk@gmail.com>
2021-04-04 17:46:27 -0700

    Initial Commit

---
+++ imu_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imu_test
+//
+//  Created by Justin Kwok Lam CHAN on 4/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
